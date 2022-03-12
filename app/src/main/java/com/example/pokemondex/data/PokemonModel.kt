ad276b03e77f2333dd53367f4175b55d69a353df
Alexandre Figueiredo <alexandrefigueiredocamargo3@gmail.com>
2022-03-12 13:52:42 -0300

    feat:#1 implements PokemonModel data class

---
+++ app/src/main/java/com/example/pokemondex/data/PokemonModel.kt
@@ -0,0 +1,9 @@
+package com.example.pokemondex.data
+
+data class PokemonModel(
+    val id: Int,
+    val name: String,
+    val height: Float,
+    val weight: Float,
+    val pokeImageUrl: String
+)
