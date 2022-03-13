d92ead5d00ed931b7bc71d63ba8edb00e913119e
Alexandre Figueiredo <alexandrefigueiredocamargo3@gmail.com>
2022-03-13 14:34:43 -0300

    feat:#3 implements Pokemonapplication class

---
+++ app/src/main/java/com/example/pokemondex/framework/di/hiltapplication/Pokemonapplication.kt
@@ -0,0 +1,12 @@
+package com.example.pokemondex.framework.di.hiltapplication
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+
+@HiltAndroidApp
+class Pokemonapplication: Application() {
+
+
+
+}
