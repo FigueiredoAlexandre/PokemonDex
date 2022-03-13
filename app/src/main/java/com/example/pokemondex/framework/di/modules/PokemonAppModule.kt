f65f24d76448a7ce1eb97f33e20279a1fe46fa6c
Alexandre Figueiredo <alexandrefigueiredocamargo3@gmail.com>
2022-03-13 15:05:27 -0300

    feat:#3 injects pokemon data source and pokemon api instances

--- app/src/main/java/com/example/pokemondex/framework/di/modules/PokemonAppModule.kt
+++ app/src/main/java/com/example/pokemondex/framework/di/modules/PokemonAppModule.kt
@@ -1,12 +1,16 @@
 package com.example.pokemondex.framework.di.modules
 
 import com.example.pokemondex.data.PokemonDataSource
+import com.example.pokemondex.data.PokemonDataSourceImp
 import com.example.pokemondex.data.PokemonRepository
 import com.example.pokemondex.data.PokemonRepositoryImp
 import com.example.pokemondex.framework.di.component.SingletonComponent
+import com.example.pokemondex.framework.remote.PokemonApi
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 
 
@@ -15,6 +19,22 @@ import javax.inject.Singleton
 object PokemonAppModule {
 
 
+    @Provides
+    @Singleton
+    fun providesPokemonApi(): PokemonApi{
+        return Retrofit.Builder()
+            .baseUrl("https://pokeapi.co/api/v2/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(PokemonApi::class.java)
+
+    }
+
+    @Provides
+    @Singleton
+    fun providesPokemonDataSource(api: PokemonApi): PokemonDataSource{
+        return PokemonDataSourceImp(api)
+    }
 
     @Provides
     @Singleton
