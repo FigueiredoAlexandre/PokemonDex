d971670f552915cd645db6f40ff4a80a3f78b2e9
Alexandre Figueiredo <alexandrefigueiredocamargo3@gmail.com>
2022-03-21 06:43:33 -0300

    feat:#5 finishes PokemonCategoryAdapter

--- app/src/main/java/com/example/pokemondex/presentation/view/adapter/PokemonCategoryAdapter.kt
+++ app/src/main/java/com/example/pokemondex/presentation/view/adapter/PokemonCategoryAdapter.kt
@@ -1,26 +1,76 @@
 package com.example.pokemondex.presentation.view.adapter
 
+import android.transition.AutoTransition
+import android.transition.TransitionManager
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.pokemondex.R
+import com.example.pokemondex.data.PokemonCategoryModel
 import com.example.pokemondex.databinding.LayoutEletricCategoryCardBinding
 
-class PokemonCategoryAdapter: RecyclerView.Adapter<PokemonCategoryAdapter.PokemonCategoryViewHolder>(){
+class PokemonCategoryAdapter(
+    val cardClickCallBack: (String) -> Unit
+): RecyclerView.Adapter<PokemonCategoryAdapter.PokemonCategoryViewHolder>(){
 
 
-    inner class PokemonCategoryViewHolder(val binding: LayoutEletricCategoryCardBinding): RecyclerView.ViewHolder(binding.root){
+    val arrayListOfCategories: ArrayList<PokemonCategoryModel> = arrayListOf()
+
+
+    inner class PokemonCategoryViewHolder(val binding: LayoutEletricCategoryCardBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(item: PokemonCategoryModel) {
+            binding.apply {
+                categoryIcon.setImageResource(item.resourceSymbol)
+                categoryLabel.text = item.name
+                expandArrow.setOnClickListener {
+                    if (expandableInfo.visibility == View.GONE) {
+
+                        TransitionManager.beginDelayedTransition(mainCardView, AutoTransition())
+                        expandableInfo.visibility = View.VISIBLE
+                        expandArrow.setImageResource(R.drawable.ic_baseline_expand_less_24)
+                        attackPro.text = item.attackQualities.attackPros
+                        attackCon.text = item.attackQualities.attackCons
+                        defensePro.text = item.defenseQualities.defensePros
+                        defenseCon.text = item.defenseQualities.defenseCons
+                    } else {
+
+                        TransitionManager.beginDelayedTransition(mainCardView, AutoTransition())
+                        expandArrow.setImageResource(R.drawable.ic_baseline_expand_more_24)
+                        expandableInfo.visibility = View.GONE
+                    }
+                }
+                categoryContainer.setOnClickListener {
+                    cardClickCallBack(item.name)
+                }
+            }
+        }
 
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PokemonCategoryViewHolder {
-        return PokemonCategoryViewHolder(LayoutEletricCategoryCardBinding.inflate(LayoutInflater.from(parent.context), parent, false))
+        return PokemonCategoryViewHolder(
+            LayoutEletricCategoryCardBinding.inflate(
+                LayoutInflater.from(
+                    parent.context
+                ), parent, false
+            )
+        )
     }
 
     override fun onBindViewHolder(holder: PokemonCategoryViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        holder.bind(arrayListOfCategories[position])
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return arrayListOfCategories.size
+    }
+
+    fun updateAdapter(list: ArrayList<PokemonCategoryModel>) {
+        arrayListOfCategories.clear()
+        arrayListOfCategories.addAll(list)
+        notifyDataSetChanged()
     }
 }
